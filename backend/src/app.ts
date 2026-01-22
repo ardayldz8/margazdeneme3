8663a73958869bd7fd97375e181371c4c758628d
ardayldz8 <ardayldz8@hotmail.com>
2026-01-22 13:37:30 +0300

    Backend: Add Netlify URL to CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 // Security Middleware
 app.use(helmet());
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://margaz.netlify.app'],
     credentials: true
 }));
 app.use(express.json());
